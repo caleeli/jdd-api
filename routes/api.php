b27c11940b24ebabcfebed936125f66f7cec3dfb
David Callizaya <davidcallizaya@gmail.com>
2019-05-30 16:42:15 -0400

    fix routes

--- routes/api.php
+++ routes/api.php
@@ -7,31 +7,29 @@ Route::group(
         'namespace' => 'JDD\Api\Http\Controllers',
     ],
     function () {
+        Route::get(
+            '/{module}/{model1?}/{id1?}/{model2?}/{id2?}/{model3?}/{id3?}/{model4?}/{id4?}/{model5?}/{id5?}',
+            ['uses' => 'ApiController@index']
+        );
+
+        Route::post(
+            '/{module}/{model1?}/{id1?}/{model2?}/{id2?}/{model3?}/{id3?}/{model4?}/{id4?}/{model5?}/{id5?}',
+            ['uses' => 'ApiController@store']
+        );
+
+        Route::patch(
+            '/{module}/{model1?}/{id1?}/{model2?}/{id2?}/{model3?}/{id3?}/{model4?}/{id4?}/{model5?}/{id5?}',
+            ['uses' => 'ApiController@update']
+        );
+
+        Route::put(
+            '/{module}/{model1?}/{id1?}/{model2?}/{id2?}/{model3?}/{id3?}/{model4?}/{id4?}/{model5?}/{id5?}',
+            ['uses' => 'ApiController@update']
+        );
+
+        Route::delete(
+            '/{module}/{model1?}/{id1?}/{model2?}/{id2?}/{model3?}/{id3?}/{model4?}/{id4?}/{model5?}/{id5?}',
+            ['uses' => 'ApiController@delete']
+        );
     }
 );
-Route::middleware('auth:api')->group(function () {
-    Route::get(
-        '/{module}/{model1?}/{id1?}/{model2?}/{id2?}/{model3?}/{id3?}/{model4?}/{id4?}/{model5?}/{id5?}',
-        ['uses' => 'ApiController@index']
-    );
-
-    Route::post(
-        '/{module}/{model1?}/{id1?}/{model2?}/{id2?}/{model3?}/{id3?}/{model4?}/{id4?}/{model5?}/{id5?}',
-        ['uses' => 'ApiController@store']
-    );
-
-    Route::patch(
-        '/{module}/{model1?}/{id1?}/{model2?}/{id2?}/{model3?}/{id3?}/{model4?}/{id4?}/{model5?}/{id5?}',
-        ['uses' => 'ApiController@update']
-    );
-
-    Route::put(
-        '/{module}/{model1?}/{id1?}/{model2?}/{id2?}/{model3?}/{id3?}/{model4?}/{id4?}/{model5?}/{id5?}',
-        ['uses' => 'ApiController@update']
-    );
-
-    Route::delete(
-        '/{module}/{model1?}/{id1?}/{model2?}/{id2?}/{model3?}/{id3?}/{model4?}/{id4?}/{model5?}/{id5?}',
-        ['uses' => 'ApiController@delete']
-    );
-});
