3a7446ae6aaa4e67949a57d29d237069eb1418d8
David Callizaya <davidcallizaya@gmail.com>
2019-05-30 11:45:02 -0400

    Add exception classes

---
+++ src/Exceptions/InvalidApiCall.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace JDD\Api\Exceptions;
+
+use Exception;
+
+/**
+ * Description of InvalidApiCall
+ *
+ * @author davidcallizaya
+ */
+class InvalidApiCall extends Exception
+{
+}
---
+++ src/Exceptions/NotFoundException.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace JDD\Api\Exceptions;
+
+use Exception;
+
+/**
+ * Description of NotFoundException
+ *
+ * @author davidcallizaya
+ */
+class NotFoundException extends Exception
+{
+}
