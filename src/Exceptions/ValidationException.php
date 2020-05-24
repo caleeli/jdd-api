fae015ad83d5dae4393224f1afc16e0a7b932be9
David Callizaya <davidcallizaya@gmail.com>
2020-05-24 09:51:58 -0400

    Add ValidationException class

---
+++ src/Exceptions/ValidationException.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace JDD\Api\Exceptions;
+
+use Exception;
+use Illuminate\Validation\ValidationException as Base;
+
+/**
+ * ValidationException with translation
+ *
+ */
+class ValidationException extends Base
+{
+
+    /**
+     * Create a new exception instance.
+     *
+     * @param  \Illuminate\Validation\ValidationException $exception
+     *
+     * @return void
+     */
+    public function __construct(Base $exception)
+    {
+        Exception::__construct(__('exceptions.ValidationException', []));
+
+        $this->response = $exception->response;
+        $this->errorBag = $exception->errorBag;
+        $this->validator = $exception->validator;
+    }
+}
